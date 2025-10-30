7869b373eb42322155132c208c9651deaa5f8b56
Chetany Kalaneya <chetany720@outlook.com>
2025-10-30 18:43:58 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
 import { useRouter } from 'next/router';
-import Logo from './Logo';
 
 const Header = () => {
   const router = useRouter();
@@ -7,9 +6,6 @@ const Header = () => {
   return (
     <div className="w-full">
       <div className="flex justify-between items-center py-4 px-4 sm:px-6 lg:px-8">
-        <div className="cursor-pointer" onClick={() => router.push("/")}>
-          <Logo />
-        </div>
       </div>
     </div>
   );
